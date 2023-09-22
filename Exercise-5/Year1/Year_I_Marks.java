a6c4e00f010eb7ebbfbfa13ab12dc53b71368e08
Navin Kumaran <87767064+Navin2k4@users.noreply.github.com>
2023-09-22 19:44:34 +0530

    Add files via upload

---
+++ Exercise-5/Year1/Year_I_Marks.java
@@ -0,0 +1,9 @@
+package Year1;
+public class Year_I_Marks {
+        public int sub1Marks;
+        public int sub2Marks;
+    public Year_I_Marks(int sub1,int sub2){
+        this.sub1Marks = sub1;
+        this.sub2Marks = sub2;
+    }
+}
---
+++ Exercise-5/Year2/Year_II_Marks.java
@@ -0,0 +1,10 @@
+package Year2;
+public class Year_II_Marks {
+    public int sub3Marks;
+    public int sub4Marks;
+
+    public Year_II_Marks(int sub3,int sub4){
+        this.sub3Marks = sub3;
+        this.sub4Marks = sub4;
+    }
+}
